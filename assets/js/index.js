a214ada69330d0a0ef3c3e90b2702fb529dd4dcd
samuelson <samuelgutierrez533@gmail.com>
2024-03-04 13:41:58 -0400

    Fix format in all the documents

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    // Get navbar-burger element
-    const navbarBurger = document.querySelector('.navbar-burger');
+document.addEventListener('DOMContentLoaded', () => {
+  // Get navbar-burger element
+  const navbarBurger = document.querySelector('.navbar-burger');
 
-    // Add click event
-    navbarBurger.addEventListener('click', () =>{
+  // Add click event
+  navbarBurger.addEventListener('click', () => {
 
-        // Get the target
-        const target = document.getElementById(navbarBurger.dataset.target);
+    // Get the target
+    const target = document.getElementById(navbarBurger.dataset.target);
 
-        // Toggle the 'is-active' on the 'navbar-burger' and the 'navbar-menu'
-        navbarBurger.classList.toggle('is-active');
-        target.classList.toggle('is-active');
-    });
+    // Toggle the 'is-active' on the 'navbar-burger' and the 'navbar-menu'
+    navbarBurger.classList.toggle('is-active');
+    target.classList.toggle('is-active');
+  });
 });
